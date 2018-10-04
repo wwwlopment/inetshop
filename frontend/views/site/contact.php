be34e0056a6e2e47deb9bb7865cb77eb13d5b00f
wwwlopment <wwwlopment@gmail.com>
2018-10-04 22:44:54 +0300

    create new theme

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -30,9 +30,8 @@ $this->title = 'Про нас';
             <h3 class="title">Як нас<span> знайти </span></h3>
             <p> <a href="https://goo.gl/maps/QakkKNVaDon"> наприклад за цим посиланням </a> </p>
         </div>
-        <iframe class="wow zoomIn animated animated" data-wow-delay=".5s"  width="600" height="450" frameborder="0" style="border:0"
-                src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJQdUbbu-ZJUcRNnapTvdbzLQ&key=" allowfullscreen></iframe>
-<!--        <iframe class="wow zoomIn animated animated" data-wow-delay=".5s" src="https://goo.gl/maps/uywJgkm89sC2" allowfullscreen=""></iframe>-->
-    </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2452.662530801811!2d25.327409854255315!3d50.7471848524825!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472599ef6e1bd541%3A0x2ea2f2660661535a!2z0KTRltC70ZbRjyDihJYxINC00LjRgtGP0YfQvtGXINC_0L7Qu9GW0LrQu9GW0L3RltC60Lg!5e0!3m2!1suk!2sua!4v1538682213154" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+        </div>
 </div>
 
